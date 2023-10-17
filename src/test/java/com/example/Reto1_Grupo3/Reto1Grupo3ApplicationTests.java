11ee25bd85e3283d4536a496a289e7bd4d69b164
Ceptorshock <ceptor90@gmail.com>
2023-10-17 18:41:05 +0200

    Estructura Base de Datos

---
+++ src/test/java/com/example/Reto1_Grupo3/Reto1Grupo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Reto1_Grupo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto1Grupo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
